ed3afc922330f86aaed536fc12c01c88c509df72
natoen <hgmatsuda@gmail.com>
2016-07-23 23:53:34 +0900

    (refactor) Revise code and add comments

--- sieve.js
+++ sieve.js
@@ -1,14 +1,26 @@
+/*
+We have an array that we will fill with false booleans
+and prime numbers. We will start at number 2 and
+check if that index in our array has a value false.
+If it hasn't then we will fill that spot with the number itself.
+We will then continue in getting its multiples until
+our limit (input n) and fill those spots with a false correspondingly.
+*/
+
 function sieve(n) {
-  var array = [], primes = [];
+  const array = [];
 
-  for (var i = 2; i <= n; i++) {
-    if (!array[i]) {
-      primes.push(i);
+  for (let i = 2; i <= n; i++) {
+    if (array[i] !== false) {
+      array[i] = i;
 
-      for (var j = i << 1; j <= n; j += i) 
-        array[j] = true;
+      for (let j = i << 1; j <= n; j += i) {
+        array[j] = false;
+      }
     }
   }
 
-  return primes;
-};
+  return array.filter((element) => element !== false);
+}
+
+sieve(20); // [ 2, 3, 5, 7, 11, 13, 17, 19 ]
